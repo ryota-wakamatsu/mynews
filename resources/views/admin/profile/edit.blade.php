7de6f770c78605b68c93c2d28a323f343cddf602
ryouta <rowing.40036@gmail.com>
2020-10-06 10:55:08 +0000

    third commit

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Profile</title>
+        
+    
+         <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+         </style>
+        
+    </head>
+    <body>
+        <h1>編集する</h1>
+    </body>
+</html>
